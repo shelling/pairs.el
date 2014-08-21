1de00d13017b7c933a68991317d17210aec7e7cf
shelling <navyblueshellingford@gmail.com>
2014-08-22 00:05:58 +0800

    Put implementation

--- pairs.el
+++ pairs.el
@@ -12,6 +12,110 @@
 
 ;;; Code:
 
+;; undefine C-o
+(define-key global-map "\C-o" nil)
+
+;; insert ()
+(define-key global-map
+  "\C-oo"
+  '(lambda ()
+     (interactive)
+     (insert "()")
+     (backward-char)))
+
+;; insert {}
+(define-key global-map
+  "\C-ou"
+  '(lambda ()
+     (interactive)
+     (insert "{}")
+     (backward-char)))
+
+;; insert ||
+(define-key global-map
+  "\C-oi"
+  '(lambda ()
+     (interactive)
+     (insert "||")
+     (backward-char)))
+
+;; insert ""
+(define-key global-map
+  "\C-od"
+  '(lambda ()
+     (interactive)
+     (insert "\"\"")
+     (backward-char)))
+
+;; insert ''
+(define-key global-map
+  "\C-os"
+  '(lambda ()
+     (interactive)
+     (insert "''")
+     (backward-char)))
+
+;; insert ->
+(define-key global-map
+  "\C-or"
+  '(lambda ()
+     (interactive)
+     (insert "->")))
+
+;; insert =>
+(define-key global-map
+  "\C-om"
+  '(lambda ()
+     (interactive)
+     (insert "=>")))
+
+;; insert ::
+(define-key global-map
+  "\C-on"
+  '(lambda ()
+     (interactive)
+     (insert "::")))
+
+;; insert <>
+(define-key global-map
+  "\C-ov"
+  '(lambda ()
+     (interactive)
+     (insert "<>")
+     (backward-char)))
+
+;; insert <%=  %>
+(define-key global-map
+  "\C-o5"
+  '(lambda ()
+     (interactive)
+     (insert "<%=  %>")
+     (backward-char 3)))
+
+;; insert <%  %>
+(define-key global-map
+  "\C-o%"
+  '(lambda ()
+     (interactive)
+     (insert "<%  %>")
+     (backward-char 3)))
+
+;; insert []
+(define-key global-map
+  "\C-ok"
+  '(lambda ()
+     (interactive)
+     (insert "[]")
+     (backward-char)))
+
+;; insert /*  */
+(define-key global-map
+  "\C-o8"
+  '(lambda ()
+     (interactive)
+     (insert "/*  */")
+     (backward-char 3)))
+
 (provide 'pairs)
 
 ;;; pairs.el ends here
