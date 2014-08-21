6059eb6013dae2cbf54c3125776ba0236af04002
shelling <navyblueshellingford@gmail.com>
2014-08-21 22:31:04 +0800

    Add pairs.el without implementation

---
+++ pairs.el
@@ -0,0 +1,17 @@
+;;; pairs.el --- keybinding of pairs insertion
+
+;; Copyright (C) 2014
+;;
+;; Author: shelling <navyblueshellingford@gmail.com>
+;; Maintainer: shelling <navyblueshellingford@gmail.com>
+;; Created 21 Aug 2014
+;; Keywords: Pairs
+;; Version: 0.1.0
+;; Package-Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
+
+;;; Code:
+
+(provide 'pairs)
+
+;;; pairs.el ends here
